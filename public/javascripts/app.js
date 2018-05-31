e02115456c78a5d082f96e415534dd975095d729
Sitharthan Vk <sitharthan.v@HCL.COM>
2018-05-31 21:08:05 +0530

    I can able to run ME_N application with different HTML file And Added Register related html and js files

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,11 +1,34 @@
-var app = angular.module('angularjsNodejsTutorial',[]);
-app.controller('myController', function($scope, $http) {
-    $scope.data = [];
-    var request = $http.get('/data');    
-    request.success(function(data) {
-        $scope.data = data;
+(function() {
+    var app = angular.module('myApp', ['ui.router']);
+    
+     app.run(function($rootScope, $location, $state, LoginService) {
+       console.clear();
+       console.log('running');
+      if(!LoginService.isAuthenticated()) {
+          $state.transitionTo('login');
+        }
     });
-    request.error(function(data){
-        console.log('Error: ' + data);
-    });
-});
+    
+    app.config(['$stateProvider', '$urlRouterProvider', 
+    function($stateProvider, $urlRouterProvider) {
+      $stateProvider
+        .state('login', {
+          url : '/login',
+          templateUrl : 'views/login/login.view.html',
+          controller : 'LoginController'
+        })
+        .state('home', {
+          url : '/home',
+          templateUrl : 'views/home/home.view.html',
+          controller : 'HomeController'
+        })
+        .state('register', {
+          url : '/register',
+          templateUrl : 'views/register/register.view.html',
+          controller : 'RegisterController'
+        });
+        
+         $urlRouterProvider.otherwise('/login');
+    }]);
+   
+  })();
