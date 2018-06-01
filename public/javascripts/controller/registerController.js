d5dddf082c61c11acdd844bd902fd212a9efba9f
Sitharthan <6356904+sitharthan@users.noreply.github.com>
2018-06-01 16:02:22 +0530

    Created factory method for to call register post api

--- public/javascripts/controller/registerController.js
+++ public/javascripts/controller/registerController.js
@@ -1,16 +1,24 @@
 //loginController-js 
 var app = angular.module('myApp');
-app.controller('RegisterController', function($scope, $rootScope, $stateParams, $state, LoginService) {
-$rootScope.title = "AngularJS Login Sample";
-$scope.formSubmit = function() {
-if(LoginService.login($scope.username, $scope.password)) {
-$rootScope.userName = $scope.username;
-$scope.error = '';
-$scope.username = '';
-$scope.password = '';
-$state.transitionTo('home');
-} else {
-$scope.error = "Incorrect username/password !";
-}   
-};    
+app.controller('RegisterController', function ($scope, $rootScope, $stateParams, $state, RegisterService,$http) {
+    console.log("i am in registerController");
+    $scope.registerSubmit = function () {
+        var params = {
+            "FirstName": $scope.firstname,
+            "LastName": $scope.lastname,
+            "UserName": $scope.username,
+            "Password": $scope.password
+        };
+        
+        //console.log("RegisterService.register(params) ",RegisterService.register(params));
+        if (RegisterService.register(params)) {
+            console.log("Registration Success");
+            $scope.error = '';
+            $state.transitionTo('home');
+        } else {
+            console.log("Registration Failed");
+            $scope.error = "Something went to wrong! - Registration Failed";
+        }
+       
+    };
 });
